dda6dc362dad8e946279772e43b420b552a127fb
Avishka-Rathnavibhushana <mailavishka@gmail.com>
2023-11-11 09:15:51 +0530

    Initialize flutter project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:pastry_shop_pos/pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pastry Shop POS',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Material(
+        child: const HomePage(),
+      ),
+    );
+  }
+}
